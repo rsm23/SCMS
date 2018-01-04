282f3c78019a123200438477d44c83b436037163
Rahmani Saif El Moulouk <cool2309@gmail.com>
2018-01-04 02:13:24 +0100

    Setting up "Laravel Scout" and "Algolia" as a search featires to the cms
    Setting up "Laravel Scout" and "Algolia" as a search featires to the cms

--- app/Post.php
+++ app/Post.php
@@ -4,10 +4,11 @@ namespace App;
 
 use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Post extends Model
 {
-    use Sluggable;
+    use Sluggable, Searchable;
 
     protected $guarded = [];
 
